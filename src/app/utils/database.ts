3cc0eacdf7697b1bcc1810009454d3601fa3da9d
Gabriel Aleksandravicius <gabriel.aleks@nano.ufrj.br>
2024-05-02 20:21:31 +0300

    Keep single connection to database

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -1,15 +1,27 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "", {
-  driverInfo: { name: "langchainjs" },
-});
+let client: MongoClient
+
 const dbName = 'docs';
 
 export async function getDatabaseConnectionToCollection(collection: string) {
-  await client.connect();
+  connectToDatabase()
   return client.db(dbName).collection(collection);
 }
 
-export async function closeDatabaseConnection() {
-  await client.close();
+async function connectToDatabase() {
+  if (!client) {
+    client = new MongoClient(process.env.MONGODB_ATLAS_URI || '', {
+      driverInfo: { name: "langchainjs" },
+    });
+
+    try {
+      await client.connect();
+      console.log('Connected to the database');
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      throw error;
+    }
+  }
+  return client.db();
 }
